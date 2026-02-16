dc318958b13944eee093be6e3b8a6f71b3c7eec0
delirium02 <darknightbs@gmail.com>
2026-02-16 16:51:15 +1100

    Added image and a button to show opening hours

--- src/home-page.js
+++ src/home-page.js
@@ -1,16 +1,19 @@
+import sweetsImageSource from "./images/honey-pancakes.jpg";
 
 function loadHomePage() {
     const content = document.getElementById("content");
 
     content.textContent = "";
 
-    const homeTitle = document.createElement("h1");
-    homeTitle.textContent = "La Comida Española";
-    content.appendChild(homeTitle);
-
-    const description = document.createElement("p");
-    description.textContent = "Authentic Spanish cuisine in the heart of the city.";
+    const description = document.createElement("h3");
+    description.classList.add("home-description");
+    description.textContent = "Authentic honey-based dishes and beverages, served with a side of bear hugs.";
     content.appendChild(description);
+
+    const subDescription = document.createElement("p");
+    subDescription.classList.add("home-sub-description");
+    subDescription.textContent = "Our honey is sourced from local beekeepers, ensuring the freshest and most flavorful experience for our customers. We are committed to sustainability and supporting our local community.";
+    content.appendChild(subDescription);
 
     const hours = [
         { day: "Monday", timeframe: "8am - 6pm" },
@@ -30,7 +33,30 @@ function loadHomePage() {
         })
     }
 
-    createItems(hours);
+    // Button to show / hide hours when clicked
+    const showHours = document.createElement("button");
+    showHours.classList.add("show-hours")
+    showHours.textContent = "See Opening Hours";
+
+    let hoursVisible = false;
+
+    showHours.addEventListener("click", () => {
+        if (!hoursVisible) {
+            (createItems(hours));
+            showHours.textContent = "Hide Hours"
+            hoursVisible = true;
+        } else {
+            loadHomePage();
+            hoursVisible = false;
+        }
+    });
+    content.appendChild(showHours);
+
+    const sweetsImage = document.createElement("img");
+    sweetsImage.src = sweetsImageSource;
+    sweetsImage.alt = "A palette of colorful sweets";
+    content.appendChild(sweetsImage);
 }
+
 
 export { loadHomePage };
